0942d3b4dbcc22e9725c8955ebafc5394cbce407
iamelofficial <102692730+iamelofficial@users.noreply.github.com>
2022-03-30 21:57:45 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,106 @@
+$(function(){
+
+    /*Слайдер ___ */
+    $('.slider__main').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        pauseOnDotsHover:false
+    });
+});
+
+/* JS scroll-top */
+const offset = 200;
+const scrollUp = document.querySelector('.scroll-top');
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// updateDashoffset
+const updateDashoffset = () => {
+    const height = document.documentElement.scrollHeight - window.innerHeight;
+};
+
+// onScrole
+window.addEventListener('scroll', () => {
+    updateDashoffset();
+
+    if (getTop() > offset) {
+        scrollUp.classList.add('scroll-top--show')
+    } else {
+        scrollUp.classList.remove('scroll-top--show')
+    }
+});
+
+// click
+scrollUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+/*
+= END JS scroll-top =
+*/
+
+/* JS mail modal */
+const modalCall = $("[data-modal]");
+const modalClose1 = $("[data-close1]");
+const modalClose2 = $("[data-close2]");
+const modalCreation = $(".modalcreation");
+
+modalCall.on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('visible');
+    $("body").addClass('no-scroll');
+});
+
+modalClose1.on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.menu');
+
+    setTimeout(function() {
+        modalParent.removeClass('visible');
+    }, 200);
+});
+
+modalClose2.on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.modalcreation');
+
+    setTimeout(function() {
+        modalParent.removeClass('visible');
+        $("body").removeClass('no-scroll');
+    }, 200);
+});
+
+
+modalCreation.on("click", function(event) {
+    let $this = $(this);
+
+    setTimeout(function() {
+        $this.removeClass('visible');
+        $("body").removeClass('no-scroll');
+    }, 200);
+});
+
+$(".modal-body").on("click", function(event) {
+    event.stopPropagation();
+});
+
+/*
+= END JS mail modal =
+*/
